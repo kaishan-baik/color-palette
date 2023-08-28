ce9e26eaad2fde1eb9e429a2f9985cee555f2273
icelyAdventures <baik.kaishan@gmail.com>
2023-08-28 21:08:38 +0800

    first commit

---
+++ src/Converter.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+const rgbConverter = (r, g, b) => {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+};
+
+export default rgbConverter;
